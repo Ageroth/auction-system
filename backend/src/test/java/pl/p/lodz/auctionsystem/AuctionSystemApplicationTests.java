238b35181c98fae3f46dc6e6b37d727f18f94e8d
216749 Krystian Domdalski <216749@edu.p.lodz.pl>
2020-06-10 22:51:59 +0200

    Initial commit

---
+++ backend/src/test/java/pl/p/lodz/auctionsystem/AuctionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.p.lodz.auctionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
