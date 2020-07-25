c7e9f8e478ecad7f5d679dc934ce498cf63d65e8
Krystian Domdalski <krystian.domdalski@gmail.com>
2020-07-25 23:15:07 +0200

    Added MailService and MessageService classes

---
+++ backend/src/main/java/pl/p/lodz/auctionsystem/mok/utils/MessageService.java
@@ -0,0 +1,18 @@
+package pl.p.lodz.auctionsystem.mok.utils;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+public class MessageService {
+
+    @Resource
+    private MessageSource messageSource;
+
+    public String getMessage(String code) {
+        return messageSource.getMessage(code, null, LocaleContextHolder.getLocale());
+    }
+}
