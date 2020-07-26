dc8c6855eb05ec41a3481505a415ba00797c7e3e
Krystian Domdalski <krystian.domdalski@gmail.com>
2020-07-26 18:08:46 +0200

    Added two more fields to the User Entity (password reset feature)

--- backend/src/main/java/pl/lodz/p/it/auctionsystem/entities/User.java
+++ backend/src/main/java/pl/lodz/p/it/auctionsystem/entities/User.java
@@ -15,49 +15,57 @@ import java.util.Collection;
 @NoArgsConstructor
 @Getter
 public class User extends BaseEntity {
-
+    
     @Id
     @SequenceGenerator(name = "UserSeqGen", sequenceName = "user_id_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "UserSeqGen")
     @Column(name = "id", nullable = false, unique = true, updatable = false)
     private Long id;
-
+    
     @Column(name = "login", nullable = false, unique = true, updatable = false, length = 32)
     private String login;
-
+    
     @Column(name = "password", nullable = false, length = 64)
     @Setter
     private String password;
-
+    
     @Column(name = "email", nullable = false, unique = true, updatable = false, length = 64)
     private String email;
-
+    
     @Column(name = "activated", nullable = false)
     @Setter
     private boolean activated;
-
+    
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
-
+    
     @Column(name = "activation_code", unique = true, length = 128)
     @Setter
     private String activationCode;
-
+    
+    @Column(name = "reset_password_code", unique = true, length = 64)
+    @Setter
+    private String resetPasswordCode;
+    
+    @Column(name = "reset_password_code_add_date")
+    @Setter
+    private LocalDateTime resetPasswordCodeAddDate;
+    
     @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.REFRESH, CascadeType.REMOVE}, mappedBy = "user")
     private Collection<UserAccessLevel> userAccessLevels = new ArrayList<>();
-
+    
     @Column(name = "first_name", table = "user_details", nullable = false, length = 32)
     @Setter
     private String firstName;
-
+    
     @Column(name = "last_name", table = "user_details", nullable = false, length = 32)
     @Setter
     private String lastName;
-
+    
     @Column(name = "phone_number", table = "user_details", nullable = false, length = 10)
     @Setter
     private String phoneNumber;
-
+    
     public User(String login, String password, String email, String firstName, String lastName, String phoneNumber) {
         super();
         this.login = login;
@@ -69,7 +77,7 @@ public class User extends BaseEntity {
         this.phoneNumber = phoneNumber;
         this.createdAt = LocalDateTime.now();
     }
-
+    
     @Override
     public boolean equals(Object object) {
         if (!(object instanceof User)) return false;
