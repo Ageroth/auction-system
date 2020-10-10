edf61c59a5311f7558e4b3fbf00bb887311a6964
Krystian Domdalski <krystian.domdalski@gmail.com>
2020-10-10 21:12:26 +0200

    Accept-Language header fix

--- backend/src/main/java/pl/lodz/p/it/auctionsystem/config/AppConfig.java
+++ backend/src/main/java/pl/lodz/p/it/auctionsystem/config/AppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 import java.util.Locale;
 
@@ -15,9 +15,9 @@ public class AppConfig implements WebMvcConfigurer {
     
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
-        sessionLocaleResolver.setDefaultLocale(Locale.US);
-        return sessionLocaleResolver;
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
     }
     
     @Override
