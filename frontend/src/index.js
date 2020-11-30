79aa1f1f75d5f372b6f5c316ef1483cd52f8ca6a
Ageroth <krystian.domdalski@gmail.com>
2020-11-30 22:24:36 +0100

    Added ConfigProvider for the internationalization of AntDesign's components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,11 @@ import App from './App'
 import {Provider} from "react-redux";
 import {PersistGate} from 'redux-persist/integration/react';
 import {persistor, store} from "./store";
-import './utils/i18n';
+import {ConfigProvider} from 'antd';
 import Interceptor from './utils/interceptor'
+import i18n from "./utils/i18n";
+import plPL from 'antd/lib/locale/pl_PL';
+import enGB from 'antd/lib/locale/en_GB';
 
 Interceptor.interceptor(store);
 
@@ -13,8 +16,13 @@ ReactDOM.render(
     <Suspense fallback={null}>
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-                <App/>
+                <ConfigProvider
+                    locale={(i18n.language ? i18n.language : window.localStorage.i18nextLng) === 'pl' ? plPL : enGB}>
+                    <App/>
+                </ConfigProvider>
             </PersistGate>
         </Provider>
     </Suspense>,
-    document.getElementById('root'));
+    document.getElementById('root')
+)
+;
