1dde9b3eb4c8d671ae566bab6359252fde9b2738
Ageroth <krystian.domdalski@gmail.com>
2020-12-09 11:28:39 +0100

    Http request methods tweaks

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -25,15 +25,15 @@ export function checkEmailAvailabilityRequest(value) {
 }
 
 export function activateUserRequest(value) {
-    return axios.post(`/users/me/activation/${value}`);
+    return axios.patch(`/users/me/activation/${value}`);
 }
 
 export function sendPasswordResetEmailRequest(payload) {
-    return axios.post('/users/me/password-reset', JSON.stringify(payload));
+    return axios.patch('/users/me/password-reset', JSON.stringify(payload));
 }
 
 export function resetPasswordRequest(value, payload) {
-    return axios.post(`/users/me/password-reset/${value}`, JSON.stringify(payload));
+    return axios.patch(`/users/me/password-reset/${value}`, JSON.stringify(payload));
 }
 
 export function searchUsersRequest(values) {
